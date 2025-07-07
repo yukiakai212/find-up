a9d502ebe4f1bf6c4e6c5887ab90f2188003c0dc
Yuki <yuki.akai.rz.212@gmail.com>
2025-07-07 11:47:51 +0700

    refactor: better test case

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,32 +1,39 @@
 import { describe, it, expect } from 'vitest';
-import { findUp } from '../src/index.js';
-import { join } from 'path';
+import module from 'node:module';
+import * as core from '../src/index.js';
+import * as esm from '../dist/index.js';
+const require = module.createRequire(import.meta.url);
+const cjs = require('../dist/index.cjs');
 
-describe('findUp', () => {
+describe.each([
+  ['Core', core],
+  ['ESM', esm],
+  ['CJS', cjs],
+])('findUp (%s)', (name, lib) => {
   it('should find package.json upward', () => {
-    const result = findUp('package.json', { basedir: import.meta.dirname });
+    const result = lib.findUp('package.json', { basedir: import.meta.dirname });
 
     expect(result).toBeTruthy();
   });
   it('should find package.json with array mode', () => {
-    const result = findUp(['package.json'], { basedir: import.meta.dirname });
+    const result = lib.findUp(['package.json'], { basedir: import.meta.dirname });
 
     expect(result).toBeTruthy();
   });
 
   it('should support regex name match', () => {
-    const result = findUp(/package\.json$/, { basedir: import.meta.dirname });
+    const result = lib.findUp(/package\.json$/, { basedir: import.meta.dirname });
     expect(result).toBeTruthy();
   });
 
   it('should not match anything', () => {
-    const result = findUp('this-not-exist', { basedir: import.meta.dirname });
+    const result = lib.findUp('this-not-exist', { basedir: import.meta.dirname });
     expect(result).toBe(undefined);
   });
 
   it('should respect custom matcher', () => {
     const matched = [];
-    const result = findUp('package.json', {
+    const result = lib.findUp('package.json', {
       basedir: import.meta.dirname,
       matcher: (path) => {
         matched.push(path);
